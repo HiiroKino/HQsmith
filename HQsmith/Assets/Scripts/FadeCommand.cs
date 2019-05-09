3c192801560a86536b07267508db521426ae5181
yuzuriha <yuzu.85yz@gmail.com>
2019-05-09 12:18:23 +0900

    TitleScene作成（UI配置,カーソル遷移,効果音実装）_木野陽彩

---
+++ HQsmith/Assets/Scripts/FadeCommand.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeCommand : MonoBehaviour
+{
+    [SerializeField]
+    Color pressText;
+
+    [SerializeField]
+    private float fadespeed = 0.1f;
+
+    bool tofade = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pressText = this.GetComponent<Text>().color;
+        pressText.a = 1;
+        GetComponent<Text>().color = pressText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tofade)
+        {
+            pressText.a += fadespeed;
+            GetComponent<Text>().color = pressText;
+            if(pressText.a >= 1)
+            {
+                tofade = false;
+            }
+        }
+
+
+        if (!tofade)
+        {
+            pressText.a -= fadespeed;
+            GetComponent<Text>().color = pressText;
+            if (pressText.a <= 0)
+            {
+                tofade = true;
+            }
+        }
+    }
+}
---
+++ HQsmith/Assets/Scripts/Fadein.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fadein : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 0.05f;
+
+    private Color ImageColor;
+
+    TitleController titleController;
+
+    float alfa;
+    float red, green, blue;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        titleController = GameObject.Find("TitleController").GetComponent<TitleController>();
+
+        red = GetComponent<Image>().color.r;
+        green = GetComponent<Image>().color.g;
+        blue = GetComponent<Image>().color.b;
+
+
+        ImageColor = this.GetComponent<Image>().color;
+        ImageColor = new Color(red, green, blue, alfa);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ImageColor.a <= 1)
+        {
+
+            ImageColor.a += speed;　//アルファ値を徐々に＋する
+
+            this.GetComponent<Image>().color = ImageColor;　//テキストへ反映させる
+
+        }
+
+        if (Input.anyKey)
+        {
+            ImageColor.a = 1;
+            this.GetComponent<Image>().color = ImageColor;
+            titleController.startPlay = true;
+        }
+
+        if(ImageColor.a >= 1 && !titleController.startPlay)
+        {
+            titleController.startPlay = true;
+        }
+    }
+}
