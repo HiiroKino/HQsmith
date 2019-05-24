0e8f4f8e2fcca280f6e697f7d101fcc3df0e8ffc
akisuke65 <akikiamd@gmail.com>
2019-05-24 16:26:27 +0900

    AI側のアニメーション

--- HQsmith/Assets/Scripts/MoveController.cs
+++ HQsmith/Assets/Scripts/MoveController.cs
@@ -10,10 +10,16 @@ public class MoveController : MonoBehaviour
     [SerializeField]
     float m_rotateSpeed = 90f;  //回転速度 度/s
 
+    [SerializeField]
+    SimpleAnimation m_simpleAnimation;
+
+    bool m_duringAnimation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_simpleAnimation = GetComponent<SimpleAnimation>();
+        m_duringAnimation = false;
     }
 
     // Update is called once per frame
@@ -24,11 +30,23 @@ public class MoveController : MonoBehaviour
 
     public void Move(float value)
     {
+        if (m_duringAnimation == false)
+        {
+            m_simpleAnimation.CrossFade("Run", 0.3f);
+        }
         transform.position += transform.forward * m_moveSpeed * value * Time.deltaTime;
     }
 
     public void Rotate(float value)
     {
         transform.Rotate(0, m_rotateSpeed * value * Time.deltaTime, 0f);
+    }
+
+    public bool DuringAnimation
+    {
+        set
+        {
+            m_duringAnimation = value;
+        }
     }
 }
